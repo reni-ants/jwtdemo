027059d5761e08490f652c429e3e6c0d9d1cbad8
reni-ants <ast0465546@163.com>
2021-04-02 14:18:37 +0800

    first commit

--- src/main/java/com/qinchy/jwtdemo/filter/HTTPBearerAuthorizeAttribute.java
+++ src/main/java/com/qinchy/jwtdemo/filter/HTTPBearerAuthorizeAttribute.java
@@ -1,24 +1,17 @@
 package com.qinchy.jwtdemo.filter;
 
-import java.io.IOException;
-  
-import javax.servlet.Filter;  
-import javax.servlet.FilterChain;  
-import javax.servlet.FilterConfig;  
-import javax.servlet.ServletException;  
-import javax.servlet.ServletRequest;  
-import javax.servlet.ServletResponse;  
-import javax.servlet.http.HttpServletRequest;  
-import javax.servlet.http.HttpServletResponse;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qinchy.jwtdemo.common.JWTHelper;
 import com.qinchy.jwtdemo.model.Audience;
 import com.qinchy.jwtdemo.model.ResultMsg;
 import com.qinchy.jwtdemo.model.ResultStatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.context.support.SpringBeanAutowiringSupport;  
-  
-import com.fasterxml.jackson.databind.ObjectMapper;  
+import org.springframework.web.context.support.SpringBeanAutowiringSupport;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * @author Administrator
